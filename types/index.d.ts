2c383c5658a6c3e062cc50e65537471f2adf4214
Luca Goslar <47827429+lucagoslar@users.noreply.github.com>
2022-02-11 21:01:31 +0100

    refactor: rename typings

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,4 +2,8 @@ type Settings = {
 	clone?: boolean;
 };
 
-type GenericArg = Record<string, unknown> | Array<Record<string, unknown>>;
+type Input = Record<string, unknown> | Array<Record<string, unknown>>;
+
+type Modifier = Record<string, unknown> | Array<Record<string, unknown>>;
+
+type Output = Partial<Input> & Partial<Modifier>;
